16f093f88e8928f21ca61ee4da82f63a88b4d995
Julian Perassi <83669055+perassijulian@users.noreply.github.com>
2025-06-20 18:09:49 -0300

    /api/reset-password gets token, validate it and redirects to /reset-password/change with token as query param

--- app/api/reset-password/route.ts
+++ app/api/reset-password/route.ts
@@ -1,11 +1,16 @@
 import { prisma } from "@/lib/prisma";
 import createResetPasswordToken from "@/lib/security/createResetPasswordToken";
+import { createHash } from "crypto";
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 import { z } from "zod";
 
-const paramsSchema = z.object({
-  email: z.string(),
+const emailSchema = z.object({
+  email: z.string().email(),
+});
+
+const tokenSchema = z.object({
+  token: z.string().length(64),
 });
 
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -13,7 +18,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(req: NextRequest) {
   try {
     const rawParams = await req.json();
-    const parsedParams = paramsSchema.safeParse(rawParams);
+    const parsedParams = emailSchema.safeParse(rawParams);
 
     if (!parsedParams || !parsedParams.data?.email)
       return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
@@ -64,6 +69,46 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ ok: true });
   } catch (error) {
     console.error("Error when calling reset-password: ", error);
+    return NextResponse.json({ error: "Server error." }, { status: 500 });
+  }
+}
+
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const rawToken = searchParams.get("token");
+
+    const parsedToken = tokenSchema.safeParse({ token: rawToken });
+    if (!parsedToken.success) {
+      console.error(
+        "Error when parsing reset-password token: ",
+        parsedToken.error
+      );
+      return NextResponse.json({ error: "Invalid token" }, { status: 400 });
+    }
+    const { token } = parsedToken.data;
+    const hashedToken = createHash("sha256").update(token).digest("hex");
+
+    const res = await prisma.passwordResetToken.findUnique({
+      where: { token: hashedToken },
+    });
+
+    if (!res?.userId) {
+      return NextResponse.json({ error: "Invalid token" }, { status: 400 });
+    }
+
+    if (res.expiresAt < new Date()) {
+      await prisma.passwordResetToken.deleteMany({
+        where: { token: hashedToken },
+      });
+      return NextResponse.json({ error: "Invalid token" }, { status: 400 });
+    }
+
+    return NextResponse.redirect(
+      `${process.env.APP_URL}/reset-password/change?token=${token}`
+    );
+  } catch (error) {
+    console.error("Error when verifying reset-password token: ", error);
     return NextResponse.json({ error: "Server error." }, { status: 500 });
   }
 }
