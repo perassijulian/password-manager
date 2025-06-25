e63845c16c18b91abbb03067890469a405c3c01d
Julian Perassi <83669055+perassijulian@users.noreply.github.com>
2025-06-25 11:50:55 -0300

    handle error from withRateLimit

--- lib/rateLimit/withRateLimit.ts
+++ lib/rateLimit/withRateLimit.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { checkRateLimit } from "./checkRateLimit";
+import { RateLimitError } from "../errors/RateLimitError";
 
 // Wrapper to rate limit by id and/or email
 export async function withRateLimit(
@@ -7,9 +8,18 @@ export async function withRateLimit(
   handler: () => Promise<NextResponse>,
   email?: string
 ): Promise<NextResponse> {
-  const rateLimitCheck = await checkRateLimit(req, email);
-  if (!rateLimitCheck.ok) {
-    return rateLimitCheck.response;
+  try {
+    await checkRateLimit(req, email);
+    return await handler();
+  } catch (error) {
+    if (error instanceof RateLimitError) {
+      return NextResponse.json(
+        { error: error.message },
+        { status: error.status }
+      );
+    }
+
+    console.error("Unhandled error in withRateLimit:", error);
+    return NextResponse.json({ error: "Server error." }, { status: 500 });
   }
-  return handler();
 }
