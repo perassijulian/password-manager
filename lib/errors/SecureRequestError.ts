2b9b22d81268db5b544340422b81fe537cac9e0a
Julian Perassi <83669055+perassijulian@users.noreply.github.com>
2025-07-02 17:13:29 -0300

    create SecureRequestError class extending Error

---
+++ lib/errors/SecureRequestError.ts
@@ -0,0 +1,30 @@
+export class SecureRequestError extends Error {
+  status: number;
+
+  constructor(message: string, status = 400) {
+    super(message);
+    this.name = "SecureRequestError";
+    this.status = status;
+  }
+}
+
+export class RateLimitError extends SecureRequestError {
+  constructor(message = "Too many requests") {
+    super(message, 429);
+    this.name = "RateLimitError";
+  }
+}
+
+export class CsrfError extends SecureRequestError {
+  constructor(message = "Invalid crsf token") {
+    super(message, 403);
+    this.name = "CsrfError";
+  }
+}
+
+export class UnauthorizedError extends SecureRequestError {
+  constructor(message = "Unauthorized") {
+    super(message, 401);
+    this.name = "UnauthorizedError";
+  }
+}
